ddb40a830e6f7a3a40aac5f7409ffd2db4bc82f7
Catherine Granic <catherinegranic@gmail.com>
2022-12-08 10:49:22 -0300

    fix base route

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: "static",
   router: {
-    base: "https://los-heroes-counter-app.onrender.com/",
+    base: "/",
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
